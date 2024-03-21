e4851269317c743fb740ffee8caf97970a716974
UTEDECIMO123 <alegzz1675@gmail.com>
2024-03-20 23:30:32 -0600

    se modificaron el diseño de login y registro

--- src/views/register.vue
+++ src/views/register.vue
@@ -1,341 +1,164 @@
 <template>
-    <div class="home">
-      <div class="wrapper fadeInDown">
-        <div id="formContent">
-          <!-- Tabs Titles -->
+  <!-- Registration Form with Shadow -->
+  <div class="bg-light py-3 py-md-5">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-5 col-xxl-4">
+          <div class="card border border-light-subtle rounded-3 shadow">
+            <!-- Agregué la clase "shadow" para aplicar sombreado -->
+            <div class="card-body p-3 p-md-4 p-xl-5">
+              <div class="text-center mb-3">
+                <a href="#!">
+                  <img src="../assets/data2.png" alt="Data Logo" width="350" height="150">
+                </a>
+              </div>
+              <h2 class="fs-6 fw-normal text-center text-secondary mb-4">Registro</h2>
+              <form v-on:submit.prevent="login">
+                <div class="row gy-2 overflow-hidden">
   
-          <!-- Icon -->
-          <div class="fadeIn first">
-            <img src="@/assets/data2.png" id="icon" alt="User Icon" />
+                  <div class="col-12">
+                    <div class="form-floating mb-3">
+                      <input type="text" class="form-control" name="username" id="username" placeholder="Username" v-model="username" required>
+                      <label for="username" class="form-label">Username</label>
+                    </div>
+                  </div>
+  
+                  <div class="col-12">
+                    <div class="form-floating mb-3">
+                      <input type="text" class="form-control" name="first_name" id="first_name" placeholder="First Name" v-model="first_name" required>
+                      <label for="first_name" class="form-label">First Name</label>
+                    </div>
+                  </div>
+  
+                  <div class="col-12">
+                    <div class="form-floating mb-3">
+                      <input type="text" class="form-control" name="last_name" id="last_name" placeholder="Last Name" v-model="last_name" required>
+                      <label for="last_name" class="form-label">Last Name</label>
+                    </div>
+                  </div>
+  
+                  <div class="col-12">
+                    <div class="form-floating mb-3">
+                      <input type="email" class="form-control" name="email" id="email" placeholder="name@example.com" v-model="email" required>
+                      <label for="email" class="form-label">Email</label>
+                    </div>
+                  </div>
+  
+                  <div class="col-12">
+                    <div class="form-floating mb-3">
+                      <input type="text" class="form-control" name="puesto" id="puesto" placeholder="Puesto" v-model="puesto" required>
+                      <label for="puesto" class="form-label">Puesto</label>
+                    </div>
+                  </div>
+  
+                  <div class="col-12">
+                    <div class="form-floating mb-3">
+                      <input type="password" class="form-control" name="password" id="password"  placeholder="Password" v-model="password" required>
+                      <label for="password" class="form-label">Password</label>
+                    </div>
+                  </div>
+  
+                  <!-- para mostrar el error -->
+                  <div class="alert alert-danger" role="alert" v-if="error">
+                      {{ error_msg }}
+                  </div>
+  
+                  <div class="col-12">
+                    <div class="d-grid my-3">
+                      <button class="btn btn-primary btn-lg" type="submit" value="Crear">Sign up</button>
+                    </div>
+                  </div>
+  
+                  <div class="col-12">
+                    <p class="m-0 text-secondary text-center">Already have an account? <a href="/" class="link-primary text-decoration-none">Sign in</a></p>
+                  </div>
+  
+                </div>
+              </form>
+            </div>
           </div>
-  
-          <!-- Login Form -->
-          <form v-on:submit.prevent="login">
-            <input type="text" id="username" class="fadeIn second" name="username" placeholder="Usuario" v-model="username">
-            <input type="text" id="first_name" class="fadeIn third" name="first_name" placeholder="Nombre" v-model="first_name">
-            <input type="text" id="last_name" class="fadeIn third" name="last_name" placeholder="Apellido" v-model="last_name">
-            <input type="email" id="email" class="fadeIn third" name="email" placeholder="email" v-model="email">
-            <input type="puesto" id="puesto" class="fadeIn third" name="puesto" placeholder="Puesto" v-model="puesto">
-            <input type="text" id="password" class="fadeIn third" name="password" placeholder="Password" v-model="password">
-            <input type="submit" class="fadeIn fourth" value="Crear">
-          </form>
-  
-          <!-- para mostrar el error -->
-          <div class="alert alert-danger" role="alert" v-if="error">
-            {{ error_msg }}
-          </div>
-  
         </div>
+      </div>
     </div>
-    </div>
-  </template>
-  
-  <script>
-  // @ is an alias to /src
-  import axios from 'axios';
-  export default {
-    name: 'HomeView',
-    components: {
-    },
-    data: function(){
-      return{
-        username: "",
-        first_name:"",
-        last_name:"",
-        email:"",
-        puesto:"",
-        password:"",
-        error: false,
-        error_msg:"",
-      }
-    },
-    methods:{
-      login(){
-        let json = {
-          "username": this.username,
-          "first_name": this.first_name,
-          "last_name":this.last_name,
-          "email": this.email,
-          "puesto": this.puesto,
-          "password":this.password
-        };
-        axios.post('http://127.0.0.1:8000/register/', json)
-        .then( data => {
-         console.log(data); // para ver si muestra datos
-            if(data.status == 201){
-                console.log("Usuario creado correctamente");
-                this.$router.push('/');
-            }else{
-                console.log("Ya existe el user");
-                }
-        })
-  
-      }
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'HomeView',
+  data: function() {
+    return {
+      username: "",
+      first_name:"",
+      last_name:"",
+      email:"",
+      puesto:"",
+      password:"",
+      error: false,
+      error_msg:"",
+    }
+  },
+  methods:{
+    login(){
+      let json = {
+        "username": this.username,
+        "first_name": this.first_name,
+        "last_name":this.last_name,
+        "email": this.email,
+        "puesto": this.puesto,
+        "password":this.password
+      };
+      axios.post('http://127.0.0.1:8000/register/', json)
+      .then(data => {
+        console.log(data); // para ver si muestra datos
+        if(data.status == 201){
+          console.log("Usuario creado correctamente");
+          this.$router.push('/');
+        }else{
+          console.log("Ya existe el usuario");
+        }
+      });
     }
   }
-  </script>
-  
-  <style scoped>
-  /* BASIC */
-  
-  html {
-    background-color: #56baed;
-  }
-  
-  body {
-    font-family: "Poppins", sans-serif;
-    height: 100vh;
-  }
-  
-  a {
-    color: #92badd;
-    display:inline-block;
-    text-decoration: none;
-    font-weight: 400;
-  }
-  
-  h2 {
-    text-align: center;
-    font-size: 16px;
-    font-weight: 600;
-    text-transform: uppercase;
-    display:inline-block;
-    margin: 40px 8px 10px 8px; 
-    color: #cccccc;
-  }
-  
-  
-  
-  /* STRUCTURE */
-  
-  .wrapper {
-    display: flex;
-    align-items: center;
-    flex-direction: column; 
-    justify-content: center;
-    width: 100%;
-    min-height: 100%;
-    padding: 20px;
-  }
-  
-  #formContent {
-    -webkit-border-radius: 10px 10px 10px 10px;
-    border-radius: 10px 10px 10px 10px;
-    background: #fff;
-    padding: 30px;
-    width: 90%;
-    max-width: 450px;
-    position: relative;
-    padding: 0px;
-    -webkit-box-shadow: 0 30px 60px 0 rgba(0,0,0,0.3);
-    box-shadow: 0 30px 60px 0 rgba(0,0,0,0.3);
-    text-align: center;
-  }
-  
-  #formFooter {
-    background-color: #f6f6f6;
-    border-top: 1px solid #dce8f1;
-    padding: 25px;
-    text-align: center;
-    -webkit-border-radius: 0 0 10px 10px;
-    border-radius: 0 0 10px 10px;
-  }
-  
-  
-  
-  /* TABS */
-  
-  h2.inactive {
-    color: #cccccc;
-  }
-  
-  h2.active {
-    color: #0d0d0d;
-    border-bottom: 2px solid #5fbae9;
-  }
-  
-  
-  
-  /* FORM TYPOGRAPHY*/
-  
-  input[type=button], input[type=submit], input[type=reset]  {
-    background-color: #56baed;
-    border: none;
-    color: white;
-    padding: 15px 80px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    text-transform: uppercase;
-    font-size: 13px;
-    -webkit-box-shadow: 0 10px 30px 0 rgba(95,186,233,0.4);
-    box-shadow: 0 10px 30px 0 rgba(95,186,233,0.4);
-    -webkit-border-radius: 5px 5px 5px 5px;
-    border-radius: 5px 5px 5px 5px;
-    margin: 5px 20px 40px 20px;
-    -webkit-transition: all 0.3s ease-in-out;
-    -moz-transition: all 0.3s ease-in-out;
-    -ms-transition: all 0.3s ease-in-out;
-    -o-transition: all 0.3s ease-in-out;
-    transition: all 0.3s ease-in-out;
-  }
-  
-  input[type=button]:hover, input[type=submit]:hover, input[type=reset]:hover  {
-    background-color: #39ace7;
-  }
-  
-  input[type=button]:active, input[type=submit]:active, input[type=reset]:active  {
-    -moz-transform: scale(0.95);
-    -webkit-transform: scale(0.95);
-    -o-transform: scale(0.95);
-    -ms-transform: scale(0.95);
-    transform: scale(0.95);
-  }
-  
-  input[type=text], input[type=email], input[type="puesto"] {
-    background-color: #f6f6f6;
-    border: none;
-    color: #0d0d0d;
-    padding: 15px 32px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 16px;
-    margin: 5px;
-    width: 85%;
-    border: 2px solid #f6f6f6;
-    -webkit-transition: all 0.5s ease-in-out;
-    -moz-transition: all 0.5s ease-in-out;
-    -ms-transition: all 0.5s ease-in-out;
-    -o-transition: all 0.5s ease-in-out;
-    transition: all 0.5s ease-in-out;
-    -webkit-border-radius: 5px 5px 5px 5px;
-    border-radius: 5px 5px 5px 5px;
-  }
-  
-  input[type=text]:focus {
-    background-color: #fff;
-    border-bottom: 2px solid #5fbae9;
-  }
-  
-  input[type=text]:placeholder {
-    color: #cccccc;
-  }
-  
-  
-  
-  /* ANIMATIONS */
-  
-  /* Simple CSS3 Fade-in-down Animation */
-  .fadeInDown {
-    -webkit-animation-name: fadeInDown;
-    animation-name: fadeInDown;
-    -webkit-animation-duration: 1s;
-    animation-duration: 1s;
-    -webkit-animation-fill-mode: both;
-    animation-fill-mode: both;
-  }
-  
-  @-webkit-keyframes fadeInDown {
-    0% {
-      opacity: 0;
-      -webkit-transform: translate3d(0, -100%, 0);
-      transform: translate3d(0, -100%, 0);
-    }
-    100% {
-      opacity: 1;
-      -webkit-transform: none;
-      transform: none;
-    }
-  }
-  
-  @keyframes fadeInDown {
-    0% {
-      opacity: 0;
-      -webkit-transform: translate3d(0, -100%, 0);
-      transform: translate3d(0, -100%, 0);
-    }
-    100% {
-      opacity: 1;
-      -webkit-transform: none;
-      transform: none;
-    }
-  }
-  
-  /* Simple CSS3 Fade-in Animation */
-  @-webkit-keyframes fadeIn { from { opacity:0; } to { opacity:1; } }
-  @-moz-keyframes fadeIn { from { opacity:0; } to { opacity:1; } }
-  @keyframes fadeIn { from { opacity:0; } to { opacity:1; } }
-  
-  .fadeIn {
-    opacity:0;
-    -webkit-animation:fadeIn ease-in 1;
-    -moz-animation:fadeIn ease-in 1;
-    animation:fadeIn ease-in 1;
-  
-    -webkit-animation-fill-mode:forwards;
-    -moz-animation-fill-mode:forwards;
-    animation-fill-mode:forwards;
-  
-    -webkit-animation-duration:1s;
-    -moz-animation-duration:1s;
-    animation-duration:1s;
-  }
-  
-  .fadeIn.first {
-    -webkit-animation-delay: 0.4s;
-    -moz-animation-delay: 0.4s;
-    animation-delay: 0.4s;
-  }
-  
-  .fadeIn.second {
-    -webkit-animation-delay: 0.6s;
-    -moz-animation-delay: 0.6s;
-    animation-delay: 0.6s;
-  }
-  
-  .fadeIn.third {
-    -webkit-animation-delay: 0.8s;
-    -moz-animation-delay: 0.8s;
-    animation-delay: 0.8s;
-  }
-  
-  .fadeIn.fourth {
-    -webkit-animation-delay: 1s;
-    -moz-animation-delay: 1s;
-    animation-delay: 1s;
-  }
-  
-  /* Simple CSS3 Fade-in Animation */
-  .underlineHover:after {
-    display: block;
-    left: 0;
-    bottom: -10px;
-    width: 0;
-    height: 2px;
-    background-color: #56baed;
-    content: "";
-    transition: width 0.2s;
-  }
-  
-  .underlineHover:hover {
-    color: #0d0d0d;
-  }
-  
-  .underlineHover:hover:after{
-    width: 100%;
-  }
-  
-  
-  
-  /* OTHERS */
-  
-  *:focus {
-      outline: none;
-  } 
-  
-  #icon {
-    width:60%;
-  }
-  
-  </style>
+}
+</script>
+
+<style scoped>
+/* Estilos adicionales existentes */
+
+/* Ajustes para los campos de entrada y etiquetas */
+input[type=text],
+input[type=email],
+input[type=password],
+input[type="puesto"],
+.form-floating input[type=text],
+.form-floating input[type=email],
+.form-floating input[type=password],
+.form-floating input[type="puesto"] {
+  background-color: #f6f6f6;
+  border: none;
+  color: #0d0d0d;
+  padding: 15px 0 15px 15px; /* Ajusta el padding para que el texto se alinee a la izquierda */
+  text-align: left; /* Alinea el texto a la izquierda */
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 5px;
+  width: 100%; /* Cambia el ancho al 100% para que ocupe todo el contenedor */
+  border: 2px solid #f6f6f6;
+  -webkit-transition: all 0.5s ease-in-out;
+  -moz-transition: all 0.5s ease-in-out;
+  -ms-transition: all 0.5s ease-in-out;
+  -o-transition: all 0.5s ease-in-out;
+  transition: all 0.5s ease-in-out;
+  -webkit-border-radius: 5px;
+  border-radius: 5px;
+}
+
+/* Ajustes para las etiquetas */
+.form-floating label {
+  padding: 0 15px; /* Ajusta el padding para que la etiqueta esté alineada a la izquierda */
+}
+
+</style>
